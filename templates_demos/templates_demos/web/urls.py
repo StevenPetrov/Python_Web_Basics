2b2b729dd815e0cfd99e631effa9861bae06d89e
Steven Petrov <91495882+StevenPetrov@users.noreply.github.com>
2022-09-27 00:04:20 +0300

    demos

--- templates_demos/templates_demos/web/urls.py
+++ templates_demos/templates_demos/web/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = ()
+from django.urls import path
+
+from templates_demos.web.views import index
+
+urlpatterns = (
+    path('', index, name='index'),
+)
--- templates_demos/templates_demos/web/views.py
+++ templates_demos/templates_demos/web/views.py
@@ -1,3 +1,12 @@
+import random
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Softuni Homepage',
+        'value': random.random()
+    }
+
+    return render(request, 'index.html', context)
